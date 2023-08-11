7c832f2b2ad9d21b2becf95d1cd17396e58fbd38
Mliguili <marcel.liguili@gmail.com>
2023-08-10 21:05:52 -0300

    alteração de código no orçamento_espelho.py

--- orcamento_espelho.py
+++ orcamento_espelho.py
@@ -12,41 +12,52 @@ class Espelho:
         self.valor_espelho()
 
     def valor_espelho(self):
-        print(f'Espelho no tamanho de {self.altura}m de altura e {self.largura}m de largura, no valor de R${self.calculo:.2f}')
+        print(f'''
+*************************************************************************************************************
+ Espelho no tamanho de {self.altura}m de Altura e {self.largura}m de Largura, no valor de R${self.calculo:.2f}                          
+*************************************************************************************************************
+''')
 
 
 def define_tipo_espelho():
-        while True:
-            opcao = input(
-                'Qual Modelo? \n 1-3mm \n 2-4mm \n 3-4mm lapidado \n 4- 4mm bisotado \n 5- retroiluminado \n 6- iluminação frontal \n')
-            if opcao == '1':
-                tipo = valor_3mm
-                return tipo
-                break
-            elif opcao == '2':
-                tipo = valor_4mm
-                return tipo
-                break
-            elif opcao == '3':
-                tipo = valor_4mm_lapidado
-                return tipo
-                break
-            elif opcao == '4':
-                tipo = valor_4mm_bisotado
-                return tipo
-                break
-            elif opcao == '5':
-                tipo = valor_4mm_retroiluminado
-                return tipo
-                break
-            elif opcao == '6':
-                tipo = valor_4mm_iluminacao_frontal
-                return tipo
-                break
-            else:
-               print('Opção invalida')
+    while True:
+        opcao = input(
+'''
+Qual Modelo? 
+(1) 3mm 
+(2) 4mm
+(3) 4mm lapidado
+(4) 4mm bisotado
+(5) retroiluminado
+(6) iluminação frontal 
+''')
 
-
+        if opcao == '1':
+            tipo = valor_3mm
+            return tipo
+            break
+        elif opcao == '2':
+            tipo = valor_4mm
+            return tipo
+            break
+        elif opcao == '3':
+            tipo = valor_4mm_lapidado
+            return tipo
+            break
+        elif opcao == '4':
+            tipo = valor_4mm_bisotado
+            return tipo
+            break
+        elif opcao == '5':
+            tipo = valor_4mm_retroiluminado
+            return tipo
+            break
+        elif opcao == '6':
+            tipo = valor_4mm_iluminacao_frontal
+            return tipo
+            break
+        else:
+            print('Opção invalida')
 
 
 def orcamento_espelhos():
